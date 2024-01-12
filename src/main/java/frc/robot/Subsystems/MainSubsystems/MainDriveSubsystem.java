d6d5d2bf29ce8636f2b7e28f52bc740dd43fe14b
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-01-11 21:05:07 -0600

    added swerve code (broken now)

--- src/main/java/frc/robot/Subsystems/MainSubsystems/MainDriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/MainSubsystems/MainDriveSubsystem.java
@@ -1,5 +1,57 @@
 package frc.robot.Subsystems.MainSubsystems;
 
-public class MainDriveSubsystem {
-    
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Constants.Constants;
+import frc.robot.Subsystems.MainSubsystems.SwerveSubsystems.*;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class MainDriveSubsystem extends CommandBase {
+  private Swerve s_Swerve;
+  private DoubleSupplier translationSup;
+  private DoubleSupplier strafeSup;
+  private DoubleSupplier rotationSup;
+  private BooleanSupplier robotCentricSup;
+
+  private SlewRateLimiter translationLimiter = new SlewRateLimiter(3.0);
+  private SlewRateLimiter strafeLimiter = new SlewRateLimiter(3.0);
+  private SlewRateLimiter rotationLimiter = new SlewRateLimiter(3.0);
+
+  public MainDriveSubsystem(
+      Swerve s_Swerve,
+      DoubleSupplier translationSup,
+      DoubleSupplier strafeSup,
+      DoubleSupplier rotationSup,
+      BooleanSupplier robotCentricSup) {
+    this.s_Swerve = s_Swerve;
+    addRequirements(s_Swerve);
+
+    this.translationSup = translationSup;
+    this.strafeSup = strafeSup;
+    this.rotationSup = rotationSup;
+    this.robotCentricSup = robotCentricSup;
+  }
+
+  @Override
+  public void execute() {
+    /* Get Values, Deadband*/
+    double translationVal =
+        translationLimiter.calculate(
+            MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.Swerve.stickDeadband));
+    double strafeVal =
+        strafeLimiter.calculate(
+            MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.Swerve.stickDeadband));
+    double rotationVal =
+        rotationLimiter.calculate(
+            MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.Swerve.stickDeadband));
+
+    /* Drive */
+    s_Swerve.drive(
+        new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed),
+        rotationVal * Constants.Swerve.maxAngularVelocity,
+        !robotCentricSup.getAsBoolean(),
+        true);
+  }
 }
