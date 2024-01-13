715cec9bf2de5dd9fca5d9ae9689c88c32d7e2c0
Jiggyraff <123276580+Jiggyraff@users.noreply.github.com>
2024-01-13 14:55:32 -0600

    Update SwerveModules.java

--- src/main/java/frc/robot/Subsystems/MainSubsystems/SwerveSubsystems/SwerveDriveSubsystems/SwerveModules.java
+++ src/main/java/frc/robot/Subsystems/MainSubsystems/SwerveSubsystems/SwerveDriveSubsystems/SwerveModules.java
@@ -7,14 +7,14 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 public class SwerveModules {
     SwerveModuleConstants frontLeftCons = new SwerveModuleConstants();
     SwerveModuleState frontLeftState = new SwerveModuleState();
-    SwerveModule frontLeft = new SwerveModule(frontLeftCons, "14");
+    SwerveModule frontLeft = new SwerveModule(frontLeftCons, "rio");
     SwerveModuleConstants frontRightCons = new SwerveModuleConstants();
     SwerveModuleState frontRightState = new SwerveModuleState();
-    SwerveModule frontRight = new SwerveModule(frontRightCons, "14");
+    SwerveModule frontRight = new SwerveModule(frontRightCons, "rio");
     SwerveModuleConstants backLeftCons = new SwerveModuleConstants();
     SwerveModuleState backLeftState = new SwerveModuleState();
-    SwerveModule backLeft = new SwerveModule(backLeftCons, "14");
+    SwerveModule backLeft = new SwerveModule(backLeftCons, "rio");
     SwerveModuleConstants backRightCons = new SwerveModuleConstants();
     SwerveModuleState backRightState = new SwerveModuleState();
-    SwerveModule backRight = new SwerveModule(backRightCons, "14");
+    SwerveModule backRight = new SwerveModule(backRightCons, "rio");
 }
