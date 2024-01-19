fe0b8b63741fb8122b9a99c3f2862999bf881426
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-01-18 18:34:48 -0600

    added motors

--- src/main/java/frc/robot/TestBot/TestRotateMotorsCommand.java
+++ src/main/java/frc/robot/TestBot/TestRotateMotorsCommand.java
@@ -1,5 +1,13 @@
 package frc.robot.TestBot;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import frc.robot.Constants.Constants;
+
 public class TestRotateMotorsCommand {
+    private  CANSparkMax motorToTurn1 = new CANSparkMax(Constants.firstMotorTestingCANID, MotorType.kBrushless);
+    private CANSparkMax motorToTurn2 = new CANSparkMax(Constants.secondMotorTestingCANID, MotorType.kBrushless);
+
     
 }
