5a419ea2e46a9a0b5817eddebf1a030836ce2092
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-01-20 11:35:47 -0600

    Rudimentary Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,18 +7,28 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.TestBot.TestDriveSubsystem;
 import frc.robot.TestBot.TestDriveCommand;
 import frc.robot.Constants;
 
 public class RobotContainer {
-
-
   private final XboxController mainStick = new XboxController(0);
   private final XboxController secondStick = new XboxController(1);
   private final TestDriveSubsystem testDriveSubsystem = new TestDriveSubsystem();
   private final TestDriveCommand testDriveCommand = new TestDriveCommand(testDriveSubsystem, mainStick, secondStick);
+    InstantCommand driveSlow = new InstantCommand(testDriveSubsystem::driveSlow, testDriveSubsystem);
+    InstantCommand turnSlow = new InstantCommand(testDriveSubsystem::turnSlow, testDriveSubsystem);
     
+  private Command driveAndTurnAutoCommand() {
+    final ParallelRaceGroup driveForTwoSlow = new ParallelRaceGroup(driveSlow, new WaitCommand(2));
+    final ParallelRaceGroup turnFor10Slow = new ParallelRaceGroup(turnSlow, new WaitCommand(10));
+    return driveForTwoSlow.andThen(turnFor10Slow).andThen(driveForTwoSlow);
+  }
+
+
   public RobotContainer() {
     configureBindings();
     if(Constants.isTestRobot) {
@@ -29,6 +39,6 @@ public class RobotContainer {
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return driveAndTurnAutoCommand(); //Commands.print("No autonomous command configured");
   }
 }
