2e4c9940f1e96410af251593653cbf82863738e4
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-01-24 19:13:00 -0600

    Update Gyroscope.java

--- src/main/java/frc/robot/Swerve/Gyroscope.java
+++ src/main/java/frc/robot/Swerve/Gyroscope.java
@@ -1,8 +1,10 @@
 package frc.robot.Swerve;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.util.sendable.Sendable;
-import com.kauailabs.navx.frc;
+import edu.wpi.first.wpilibj.SPI;
+
+import com.kauailabs.navx.frc.AHRS;
 
 public class Gyroscope {
-    
+    AHRS gyro = new AHRS(SPI.Port.kMXP);
+
 }
